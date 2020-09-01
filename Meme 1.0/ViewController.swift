e2316c820287da3498c111aea2a2289a955636a4
elina-mns <ellie.mansurova@gmail.com>
2020-09-01 14:52:31 -0400

    Initial Commit

---
+++ Meme 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme 1.0
+//
+//  Created by Elina Mansurova on 2020-09-01.
+//  Copyright © 2020 Elina Mansurova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
